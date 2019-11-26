0fc008b7b68c84c9339fc03438c7820f5aac3f87
Sibylle Eloy <sibylleeloy@hotmail.com>
2019-11-26 17:49:55 +0100

    controller messages update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,25 @@
 class MessagesController < ApplicationController
-  def index
-    @messages = Message.all
-  end
-
   def new
-
+    @message = Message.new
+    @user = User.find(params[:user_id])
   end
 
   def create
+    @message = Message.new(message_params)
+    @message.sender = current_user
+    @message.recipient = User.find(params[:receiver_id])
+    raise
+      if @message.save!
+        flash[:success] = "message sent!"
+        redirect_to root_url
+      else
+        render 'new'
+      end
+  end
 
+  private
+
+  def message_params
+    params.require(:message).permit(:text)
   end
 end

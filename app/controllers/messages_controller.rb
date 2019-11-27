eb2bd4ea6b491186d480a6db4bd725945e6e0e3c
Sibylle Eloy <sibylleeloy@hotmail.com>
2019-11-27 11:54:37 +0100

    chat session first step

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,13 +7,12 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     @message.sender = current_user
-    @message.recipient = User.find(params[:receiver_id])
-    raise
+    @message.recipient = User.find(params[:user_id])
       if @message.save!
         flash[:success] = "message sent!"
-        redirect_to root_url
+        redirect_to new_user_message_path
       else
-        render 'new'
+        redirect_to new_user_message_path
       end
   end
 

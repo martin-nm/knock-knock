316e32456dd44cd1a2ca4c6ab51dd56a82c66c34
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2019-11-27 15:08:49 +0100

    continue flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
        lat: user.latitude,
        lng: user.longitude,
        infoWindow: render_to_string(partial: "info_window", locals: { user: user }),
-       image_url: helpers.asset_url('aline.jpeg'),
+       image_url: helpers.asset_url('laptop.svg'),
       }
     end
   end

e5a39482bf2dc5ab2d4a19e630d425e882e170f7
Sibylle Eloy <sibylleeloy@hotmail.com>
2019-11-28 10:37:56 +0100

    css container fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_153156) do
+ActiveRecord::Schema.define(version: 2019_11_27_150642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,15 +34,6 @@ ActiveRecord::Schema.define(version: 2019_11_27_153156) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "excludeds", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "message_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["message_id"], name: "index_excludeds_on_message_id"
-    t.index ["user_id"], name: "index_excludeds_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -87,8 +78,6 @@ ActiveRecord::Schema.define(version: 2019_11_27_153156) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "excludeds", "messages"
-  add_foreign_key "excludeds", "users"
   add_foreign_key "messages", "users", column: "recipient_id"
   add_foreign_key "messages", "users", column: "sender_id"
   add_foreign_key "posts", "users"

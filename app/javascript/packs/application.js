9cb37252bb30de12b940cea72251a07f8102e6db
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2019-11-29 11:53:06 +0100

    geoloc

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,8 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { loadDynamicBannerText } from '../components/banner';
 import { initMapbox } from '../plugins/init_mapbox';
-import { initAutocomplete } from '../plugins/init_autocomplete';
 
 initMapbox();
-initAutocomplete();
-
 
 if (document.querySelector('#banner-typed-text')) {
   loadDynamicBannerText();
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -4,10 +4,10 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-  const fitMapToMarkers = (map, markers) => {
+  const fitMapToMarkers = (map, markers, zoom) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+    map.fitBounds(bounds, { padding: 70, maxZoom: zoom, duration: 0 });
   };
 
   if (mapElement) { // only build a map if there's a div#map to inject into
@@ -32,12 +32,65 @@ const initMapbox = () => {
           .addTo(map);
       });
 
-    fitMapToMarkers(map, markers);
+    fitMapToMarkers(map, markers, 14);
 
     map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
                                           mapboxgl: mapboxgl }));
+
+    const geolocMe = (zoom) => {
+      navigator.geolocation.getCurrentPosition(e => {
+      const latitude = e.coords.latitude;
+      const longitude = e.coords.longitude;
+      console.log(latitude, longitude)
+      new mapboxgl.Marker()
+        .setLngLat([ longitude, latitude ])
+        .addTo(map);
+
+      fitMapToMarkers(map, [{lng: longitude, lat: latitude}], zoom)
+    });
+    }
+
+    document.querySelector('#localize').addEventListener('click', (event) => {
+      geolocMe(13);
+      // Call ajax vers l'index avec comme params latitude, longitude, range
+
+      // Dans l'action index du controller
+      // Si j'ai des params latitude et longitude, alors filtrer les users dans l'index avec la méthode .near
+
+      // Dans la vue js.erb, render les cards filtrées
+    });
+
+    // Au click sur le bouton 5km
+    // J'appelle geolocMe(13)
+    //
   }
 };
 
 
 export { initMapbox };
+
+// faire un bouton pour ecouter l event, geoloc a ce moment la, recup la long et lat
+// puis les donner à un marker appelé my marker et fiter la mpa sur ce marker
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

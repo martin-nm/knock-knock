2497fcc5369d3736363ea1a89c1a32672c10794a
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2019-12-03 16:23:41 +0100

    footer fixed

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["interract with co-workers", "get help from experts"],
+    strings: ["interact with co-workers", "get help from experts"],
     typeSpeed: 60,
     loop: true,
     showCursor: false,
